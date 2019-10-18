88751afbfc3fb4f9ea119dacabc9df4ed796f4c9
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2019-10-19 03:19:57 +1000

    Create variables.js
    Working with variables - declaring and initializing variables in JavaScript.

---
+++ js_concepts/variables.js
@@ -0,0 +1,16 @@
+/*
+  Variables are the most basic form of storage unit in JavaScript (and any other programming language).
+  
+  Variables allow certain assigned values to be re-used and referenced multiple times throughout a script. Variables can only hold one
+  value at a time.
+  
+  Variables can be created in two ways - variable "declaration" and variable "initialization". All variables in JavaScript are created using
+  the "var" keyword, followed by the name of the variable, the assignment operator, and the value of the variable. JavaScript uses
+  "Camel Casing" as a standard for naming variables.
+*/
+
+// Declaring a variable
+var myVariable; // No value is assigned to "myVariable" - By default, it will contain the value of "null".
+
+// Initializing a variable
+var anotherVariable = "Hello, world"; // Variables that are initialized have "initial" values assigned to them, as this variable.
