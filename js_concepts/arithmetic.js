ad77aa8d8d5e8ba87c58c796684d62ae8bb9fe54
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2019-10-19 03:42:24 +1000

    Arithmetic and increment/decrement operators
    Working with the five arithmetic and the increment/decrement operators in JavaScript.

---
+++ js_concepts/arithmetic.js
@@ -0,0 +1,50 @@
+/*
+  JavaScript provides the following five (5) common arithmetic operations:
+  1. Addition (+) - Adds operands
+  2. Subtraction (-) - Subtracts operands
+  3. Multiplication (*) - Multiplies operands
+  4. Division (/) - Divides operands
+  5. Modulus (%) - Finds the remainder of operands
+*/
+
+// Addition operation (with variables)
+var num1 = 5;
+var num2 = 2;
+
+var add = num1 + num2; // "add" = 7
+var addition = 3 + 9; // "addition" = 12
+
+// Subtraction operation (with variables)
+var subtract = num1 - num2; // Using the same variables "num1" and "num2", "subtract" = 3
+var subtraction = 8 - 7; // "subtraction" = 1
+
+// Multiplication operation (with variables)
+var multiply = num1 * num2; // "multiply" = 10
+var multiplication = 3 * 5; // "multiplication" = 15
+
+// Division operation (with variables)
+var int1 = 25;
+var int2 = 5;
+
+var divide = int1 / int2; // "divide" = 5
+var division = 21 / 7; // "division" = 3
+
+// Modulus operation (with variables)
+var remainder = num1 % num2; // "remainder" = 1
+var modulus = 18 / 6; // "modulus" = 0
+
+
+
+/*
+  Increment and Decrement Operator in JavaScript:
+  1. Increment Operator (++) - This operator increases an integer value by 1
+  2. Decrement Operator (--) - This operator is the opposite of the increment operator. It decreases an integer value by 1
+*/
+
+// Incrementing a value
+var val1 = 7;
+val1++; // "val1" = 8
+
+// Decrementing a value
+var val2 = 5;
+val2--; // "val2" = 4
