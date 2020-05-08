de4eb834c42a1f3c2296364dc81943625b780a3a
cravenhoff <crystalkewe@gmail.com>
2020-05-09 06:13:26 +1000

    Add innerHTML using elements

--- js_concepts/outputs.js
+++ js_concepts/outputs.js
@@ -1,14 +1,15 @@
 /*
-  
+
   JavaScript can output or display data in the following ways:
   1. innerHTML - Displaying data in a HTML element
   2. document.write() - Displaying data onto the HTML display
   3. window.alert() - Outputting data onto an alert box
   4. console.log() - Outputting data onto the browser console
-  
+
 */
 
 // Using the innerHTML display/output
+document.body.innerHTML = "Howdy!"; // Replaces the value of the body tag/element with the string "Howdy"
 document.getElementById("name").innerHTML = "John Doe"; /* This statement outputs the string <John Doe> into the HTML element
                                                            containing the id "name" */
 
@@ -22,4 +23,4 @@ window.alert("Saying HELLO to the world!"); // Outputs the string <Saying HELLO 
 console.log("Console output"); // Outputs the string <Console output> onto the browser console
 
 var hello = "Hello, world!";
-console.log(hello); // Outputs the string <Hello, world!> onto the console 
+console.log(hello); // Outputs the string <Hello, world!> onto the console
