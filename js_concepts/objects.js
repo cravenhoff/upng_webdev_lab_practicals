60378be44ad10e574e2f536d39a0ecb306e08765
cravenhoff <crystalkewe@gmail.com>
2020-05-08 23:52:51 +1000

    Access object properties
    How to access object properties using dot and bracket notation.

--- js_concepts/objects.js
+++ js_concepts/objects.js
@@ -20,3 +20,44 @@ var exampleObject = {
 
 // The properties of the object are strings that precede the colon, and their values follow after the colon. Each property is
 // separated by a comma, and the values of the properties are various data types – strings, numbers and even an array.
+
+/*
+
+  Accessing Object Properties Using "Dot Notation"
+
+  There are two methods of accessing object properties, the first is dot notation. Object properties are easily accessed by
+  calling the object using its name, followed by a dot and the name of the property.
+
+*/
+
+// How to access an object's properties using dot_notation
+var foodObject = {
+  “name” : “Chicken & Chips”,
+  “cost” : “K20.00”,
+  “quantity” : 1,
+  “restaurant” : “The Smithy’s”
+};
+
+// Accessing the "name" and "restaurant" properties of the foodObject{}.
+var food = foodObject.name; // food = “Chicken & Chips”.
+var restaurant = foodObject.restaurant; // restaurant = “The Smithy’s”.
+
+/*
+
+  Accessing Object Properties with "Bracket Notation"
+
+  The second method for accessing object properties is bracket notation. Bracket notation can be used to access any type of
+  object property, but it is required when the properties have space in them, or in other words, are composed of more than
+  one word.
+
+*/
+
+// How to access an object's properties using bracket_notation
+var menu = {
+  “the entree” : “Chicken soup with bread”,
+  “main course” : “Grilled beef with mashed potatoes”,
+  “desert” : “Tropical coconut cake”
+};
+// Accessing the entree and main course properties of the menu{}.
+var entree = menu[“entree”]; // Returns entree = "Chicken soup with bread"
+var mainCourse = menu[“main course”]; // Returns mainCourse = "Grilled beef with mashed potatoes"
