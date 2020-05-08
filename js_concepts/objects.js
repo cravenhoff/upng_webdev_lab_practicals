c4ceb9e75e411a1741e6a5e0237be0a94a4c66d5
cravenhoff <crystalkewe@gmail.com>
2020-05-08 23:37:06 +1000

    Add objects definition and first sample
    First lesson on objects - How to Build Objects.

--- js_concepts/objects.js
+++ js_concepts/objects.js
@@ -1,1 +1,22 @@
+/*
 
+Objects are a dimension higher than arrays. Despite the similarities it shares with arrays, objects do one fundamental thing
+differently – objects store property-value pairs, unlike arrays that store index-value pairs.
+
+Objects are created in a similar fashion to regular variables and arrays.
+
+Properties of objects are enclosed within opening and closing curly braces. Properties are separated by commas and can store
+any data type as its value.
+
+*/
+
+// Example Object
+var exampleObject = {
+  “name” : “Example Object”,
+  “type” : “Object”,
+  “year_created” : 2020,
+  “favoriteColors” : [“Purple”, “Green”, “Blue”]
+};
+
+// The properties of the object are strings that precede the colon, and their values follow after the colon. Each property is
+// separated by a comma, and the values of the properties are various data types – strings, numbers and even an array.
