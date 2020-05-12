0a809c34e81ba2dae46ce63044364f4645713238
cravenhoff <crystalkewe@gmail.com>
2020-05-13 00:30:36 +1000

    Create recordcollection_objects.js
    Instructions for the exercise and a sample record collection object for reference.

---
+++ js/js_concepts/exercises/recordcollection_objects.js
@@ -0,0 +1,24 @@
+/*
+
+  Create a record collection object where each record contains a unique ID that holds the following pieces of
+  information - these will be properties of the object:
+
+  1. Album
+  2. Artist
+  3. Release_Year
+  4. (Array of) Tracks
+
+  Provided below is a sample record collection object containing the above specific properties.
+
+*/
+
+// Sample record collection object
+var collection = {
+  001: {
+    "Album" : "Thriller",
+    "Artist" : "Michael Jackson",
+    "Release_Year" : 1982,
+    "Tracks" : ["Billie Jean", "Thriller", "Beat It"]
+  }
+  // Add more record collections...
+};
